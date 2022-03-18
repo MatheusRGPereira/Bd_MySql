55e0abfef184428260f19854a7c5ecf057a3b1b6
Matheus <matheusgaldinoinfo@gmail.com>
2022-03-18 13:26:10 -0300

    Atividades BD Script MySql

---
+++ Atividade 3 Escola/Atualizacao_notas.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE db_escola;
+
+USE db_escola;
+
+CREATE TABLE tb_Informacoes_alunos(
+id BIGINT AUTO_INCREMENT,
+nome varchar(255),
+idade int,
+turma int,
+sala int,
+notas decimal (3,1),
+PRIMARY KEY (id)
+);
+
+INSERT INTO tb_informacoes_alunos (nome, idade, turma, sala, notas)
+VALUES ("Lucas",7,4,12,7.5); 
+
+INSERT INTO tb_informacoes_alunos (nome, idade, turma, sala, notas)
+VALUES ("André",7,4,12,8.5); 
+
+INSERT INTO tb_informacoes_alunos (nome, idade, turma, sala, notas)
+VALUES ("julia",8,4,12,9.5); 
+
+INSERT INTO tb_informacoes_alunos (nome, idade, turma, sala, notas)
+VALUES ("Maria luiza",9,5,14,6.5); 
+
+INSERT INTO tb_informacoes_alunos (nome, idade, turma, sala, notas)
+VALUES ("Carlos",10,5,14,5.5); 
+
+INSERT INTO tb_informacoes_alunos (nome, idade, turma, sala, notas)
+VALUES ("Ana Paula",9,6,15,9.5); 
+
+INSERT INTO tb_informacoes_alunos (nome, idade, turma, sala, notas)
+VALUES ("Felipe",12,8,17,4.5); 
+
+INSERT INTO tb_informacoes_alunos (nome, idade, turma, sala, notas)
+VALUES ("Patricia",12,8,17,10.0); 
+
+-- Atualização das notas
+UPDATE tb_informacoes_alunos set notas = 8.0 WHERE id =2; 
+
+-- Impressão da tabela completa
+SELECT * FROM tb_informacoes_alunos;
